180412b34829ca5bbe5fdbf29cecd7465f24353e
MMulla <zore.mulla@gmail.com>
2019-10-14 19:44:21 +0300

    merged meh

--- guilds/views.py
+++ guilds/views.py
@@ -1,23 +1,43 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from .serializers import GuildSerializer, DetailSerializer, CreateSerializer
-from .models import Guild
+from rest_framework.generics import CreateAPIView, ListAPIView, UpdateAPIView
+from .serializers import GuildSerializer, DetailSerializer, CreateSerializer, QuestionSerializer
+from .models import Guild, Question
 
 # Create your views here.
+
+
 class GuildView(ModelViewSet):
-	queryset = Guild.objects.all()
+    queryset = Guild.objects.all()
 
-	def get_serializer_class(self):
-		if self.action == "list":
-			return  GuildSerializer
-		if self.action == "detail":
-			return DetailSerializer
-		if self.action == "create":
-			return CreateSerializer
-		if self.action == "update":
-			return DetailSerializer
+    def get_serializer_class(self):
+        if self.action == "list":
+            return GuildSerializer
+        if self.action == "detail":
+            return DetailSerializer
+        if self.action == "create":
+            return CreateSerializer
+        if self.action == "update":
+            return DetailSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(master=self.request.user.profile)
 
 
-	def perform_create(self, serializer):
-		serializer.save(master=self.request.user.profile)
-	
+# create
+class QuestionCreateAPIView(CreateAPIView):
+    serializer_class = QuestionSerializer
+
+# Questions
+
+
+class QuestionsView(ListAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+# update
+
+
+class QuestionsUpdateView(UpdateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
